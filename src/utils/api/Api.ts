6b1108ece6f7390299e18dd17e5e122fa645e3c8
Minyeong Jeong <megormin@gmail.com>
2025-01-06 11:23:23 +0100

    util for api added

---
+++ src/utils/api/Api.ts
@@ -0,0 +1,10 @@
+export const apiKey = import.meta.env.VITE_API_KEY;
+
+// export const BASE_URL: string = "https://api.openweathermap.org/data/2.5/";
+
+// export const getCurrentURL = (cityName: string, isCelsius: boolean): string => {
+//   const resultURL = `${BASE_URL}weather?q=${cityName}&appid=${apiKey}&units=${
+//     isCelsius ? "metric" : "imperial"
+//   }`;
+//   return resultURL;
+// };
