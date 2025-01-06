59e1d4a51d5558ed060644b1e14cbf45927261df
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-01-06 10:14:24 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import "./App.css";
+import IntroApp from "./pages/introApp/IntroApp";
+import Home from "./pages/home/Home";
+import SearchPage from "./pages/searchPage/SearchPage";
+import MovieDetails from "./pages/movieDetails/MovieDetails";
+
+function App() {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <>
+                <Route path="/" element={<IntroApp />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/search_page" element={<SearchPage />} />
+                <Route path="/details" element={<MovieDetails />} />
+            </>
+        )
+    );
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/introApp/IntroApp.tsx
@@ -0,0 +1,5 @@
+const IntroApp = () => {
+    return <></>;
+};
+
+export default IntroApp;
