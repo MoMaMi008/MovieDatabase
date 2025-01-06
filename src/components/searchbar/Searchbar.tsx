765623f645f43f22f1cfd25d90d12a955bb9c995
Minyeong Jeong <megormin@gmail.com>
2025-01-06 14:26:31 +0100

    searchbar logic done

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import { getSearchUrl, OPTIONS } from "../../utils/api/Api";
+
+interface ISearchData {
+  page: number;
+  results: IMovieHome[];
+  total_pages: number;
+  total_results: number;
+}
+export interface IMovieHome {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 const Searchbar = () => {
+  const [input, setInput] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const searchData = useFetch<ISearchData>(
+    getSearchUrl(input, "en-US"),
+    OPTIONS
+  );
+
+  console.log(searchData);
+
   return (
     <section>
-      <h1>Searchbar</h1>
+      <div className="input-box">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => handleChange(e)}
+          placeholder="Search Movie ..."
+        />
+        <p>🔍</p>
+      </div>
+      {input.length > 0 && (
+        <div className="search-output">
+          {" "}
+          <ul>
+            {searchData.data?.results.map((singleMovie: IMovieHome) => (
+              <a href="#">
+                <li>{singleMovie.title}</li>
+              </a>
+            ))}
+          </ul>{" "}
+        </div>
+      )}
     </section>
   );
 };
