306236d447523382b1b346dcf0afea0a8b5f8f03
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-01-07 15:55:00 +0100

    finish: IntroApp

--- src/pages/introApp/IntroApp.tsx
+++ src/pages/introApp/IntroApp.tsx
@@ -1,14 +1,29 @@
 import SplashScreen from "../../components/splashScreen/SplashScreen";
-import background from "../../assets/images/intro-background.png";
 import img from "../../assets/images/intro.png";
 import Button from "../../components/button/Button";
 import "./IntroApp.css";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 
 const IntroApp = () => {
+    const [showIntro, setShowIntro] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowIntro(false);
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
-            {/* <SplashScreen /> */}
-            <section className="intro">
+            {showIntro && (
+                <motion.div className="splashScreen" initial={{ scale: 1, opacity: 1 }} animate={{ scale: 4, opacity: 0 }} transition={{ delay: 1, duration: 2, ease: "easeInOut" }}>
+                    <SplashScreen />
+                </motion.div>
+            )}
+            <main className="intro">
                 <div>
                     <img src={img} alt="" />
                 </div>
@@ -17,9 +32,10 @@ const IntroApp = () => {
                         Enjoy Your Movie <br /> Watch Everywhere
                     </h1>
                     <p>Stream unlimited movies and TV shows on your phone,tablet, laptop, and TV.</p>
-                    <Button text="Get Started" />
+                    <Button text="Get Started" link_path="/home" />
                 </article>
-            </section>
+            </main>
+            <div className="test"></div>
         </>
     );
 };
