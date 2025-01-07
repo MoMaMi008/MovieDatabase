e7215b2c97df45e3c58a34ffe88c7848625a5a1d
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-01-07 14:39:43 +0100

    add: link_path param to Button.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,14 +1,22 @@
 import { FC } from "react";
 import "./Button.css";
+import { useNavigate } from "react-router-dom";
 
 interface ButtonProps {
     img_path?: string;
     text: string;
+    link_path: string;
 }
 
-const Button: FC<ButtonProps> = ({ img_path, text }) => {
+const Button: FC<ButtonProps> = ({ img_path, text, link_path }) => {
+    const navigate = useNavigate();
+
+    function handleClick() {
+        navigate(link_path);
+    }
+
     return (
-        <button className="button">
+        <button className="button" onClick={handleClick}>
             {img_path && <img src={img_path} alt="button symbol" />}
             <p>{text}</p>
         </button>
