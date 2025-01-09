8c4d892894e8f3956d05b0ed14cdbad4e8788745
Minyeong Jeong <megormin@gmail.com>
2025-01-09 10:58:57 +0100

    zwischen speichern

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,37 +1,42 @@
 import "./Footer.css";
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import DefaultHomeIcon from "../../assets/SVG/Home.svg";
+import { NavLink } from "react-router-dom";
+
 import VectorIcon from "../../assets/SVG/Vector.svg";
 import ProfileIcon from "../../assets/SVG/Profile.svg";
 import "./Footer.css";
-import BookMarkIcon from "../../assets/SVG/bookMark.svg";
-import HomeIcon from "../../assets/SVG/homered.svg";
+
+import NavHomeIcon from "../../assets/SVG/navHomeIcon";
+import BookmarkIcon from "../../assets/SVG/BookmarkIcon";
 
 const Footer: React.FC = () => {
-    const location = useLocation();
-    const isHome = location.pathname === "/home";
+  return (
+    <footer className="footer">
+      <div className="footer-icons">
+        <NavLink to="/home" className="home-icon">
+          <div className="icon-box home-active">
+            {" "}
+            <NavHomeIcon />
+            <p>Home</p>
+          </div>
+        </NavLink>
+        <NavLink to="/bookmarks" className="bookmark-icon">
+          <div className="icon-box bookmark-active">
+            {" "}
+            <BookmarkIcon />
+            <p className="bookmarks-text">Bookmarks</p>
+          </div>
+        </NavLink>
 
-    return (
-        <footer className="footer">
-            <div className="footer-icons">
-                <Link to="/home" className="home-icon">
-                    <img src={isHome ? HomeIcon : DefaultHomeIcon} alt="Home" />
-                </Link>
-                <div className="bookmark-icon">
-                    <Link to="/bookmarks">
-                        <img src={BookMarkIcon} alt="Bookmarks" />
-                    </Link>
-                </div>
-                <div className="vector-icon">
-                    <img src={VectorIcon} alt="Vector" />
-                </div>
-                <div className="profile-icon">
-                    <img src={ProfileIcon} alt="Profile" />
-                </div>
-            </div>
-        </footer>
-    );
+        <div className="vector-icon">
+          <img src={VectorIcon} alt="Vector" />
+        </div>
+        <div className="profile-icon">
+          <img src={ProfileIcon} alt="Profile" />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
