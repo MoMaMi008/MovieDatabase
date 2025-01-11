3ad1752104746425ab1c426316e014b437ceff7c
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-01-11 15:30:25 +0100

    add: BackToTopButton.tsx

--- src/components/backToTopButton/BackToTopButton.tsx
+++ src/components/backToTopButton/BackToTopButton.tsx
@@ -1,18 +1,28 @@
-import ArrowIcon from "../../assets/SVG/Frame.svg";
+import React from "react";
+import ArrowUp from "../../assets/SVG/Arrow-Up.svg";
+import "./BackToTopButton.css";
 
-const BackToTopButton = () => {
-    function backToTop() {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-    }
+interface BackToTopButtonProps {
+    listRef: React.RefObject<HTMLDivElement>;
+}
+
+const BackToTopButton = React.forwardRef<HTMLButtonElement, BackToTopButtonProps>(({ listRef }, ref) => {
+    const backToTop = () => {
+        if (listRef.current) {
+            listRef.current.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+    };
 
     return (
-        <button className="backToTopButton" onClick={backToTop}>
-            <img src={ArrowIcon} alt="Arrow Icon" />
+        <button className="backToTopButton" onClick={backToTop} ref={ref}>
+            <img src={ArrowUp} alt="Arrow Icon" />
+            <p>Back to Top</p>
+            <img src={ArrowUp} alt="Arrow Icon" />
         </button>
     );
-};
+});
 
 export default BackToTopButton;
