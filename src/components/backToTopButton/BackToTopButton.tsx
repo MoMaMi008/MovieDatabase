e6913a90f7fde84fffc383c4a9df59b0ba4cf2c7
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-01-11 17:58:47 +0100

    add: Logik für Ein-/Ausblenden

--- src/components/backToTopButton/BackToTopButton.tsx
+++ src/components/backToTopButton/BackToTopButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArrowUp from "../../assets/SVG/Arrow-Up.svg";
 import "./BackToTopButton.css";
 
@@ -7,21 +7,45 @@ interface BackToTopButtonProps {
 }
 
 const BackToTopButton = React.forwardRef<HTMLButtonElement, BackToTopButtonProps>(({ listRef }, ref) => {
-    const backToTop = () => {
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    function toggleVisibility() {
+        if (listRef.current) {
+            if (listRef.current.scrollTop > 100) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        }
+    }
+
+    function backToTop() {
         if (listRef.current) {
             listRef.current.scrollTo({
                 top: 0,
                 behavior: "smooth",
             });
         }
-    };
+    }
+
+    useEffect(() => {
+        const list = listRef.current;
+        if (list) {
+            list.addEventListener("scroll", toggleVisibility);
+            return () => {
+                list.removeEventListener("scroll", toggleVisibility);
+            };
+        }
+    }, [listRef]);
 
     return (
-        <button className="backToTopButton" onClick={backToTop} ref={ref}>
-            <img src={ArrowUp} alt="Arrow Icon" />
-            <p>Back to Top</p>
-            <img src={ArrowUp} alt="Arrow Icon" />
-        </button>
+        isVisible && (
+            <button className="backToTopButton" onClick={backToTop} ref={ref}>
+                <img src={ArrowUp} alt="Arrow Icon" />
+                <p>Back to Top</p>
+                <img src={ArrowUp} alt="Arrow Icon" />
+            </button>
+        )
     );
 });
 
