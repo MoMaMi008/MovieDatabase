a6adda6a736c6dfdf62a0ec2d62cc9c556d50ee3
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-01-11 15:28:17 +0100

    edit: Anpassung für die weitergabe des Ref's

--- src/components/movieList/MovieList.tsx
+++ src/components/movieList/MovieList.tsx
@@ -1,62 +1,57 @@
 import useFetch from "../../hooks/useFetch";
 import { IMovieHome, ISearchData } from "../../pages/home/Home";
 import { getMovieListUrl, OPTIONS } from "../../utils/api/Api";
-import { FC, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 
 import { IGenre } from "../genreSlider/GenreSlider";
 import MovieCard from "../movieCard/MovieCard";
 
 interface IMovieListProps {
-  inputGenre: IGenre;
+    inputGenre: IGenre;
+    ref: React.RefObject<HTMLDivElement>;
 }
 
-const MovieList: FC<IMovieListProps> = ({ inputGenre }) => {
-  const [movies, setMovies] = useState<IMovieHome[]>([]);
-  const [page, setPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(0);
-  const [shouldFetch, setShouldFetch] = useState<boolean>(true);
+const MovieList = React.forwardRef<HTMLDivElement, IMovieListProps>((props, ref) => {
+    const [movies, setMovies] = useState<IMovieHome[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(0);
+    const [shouldFetch, setShouldFetch] = useState<boolean>(true);
 
-  const fetchUrl = shouldFetch ? getMovieListUrl(inputGenre?.id, page) : null;
-  const { data } = useFetch<ISearchData>(fetchUrl, OPTIONS);
+    const fetchUrl = shouldFetch ? getMovieListUrl(props.inputGenre?.id, page) : null;
+    const { data } = useFetch<ISearchData>(fetchUrl, OPTIONS);
 
-  useEffect(() => {
-    if (data) {
-      setMovies((prevMovies) =>
-        page === 1 ? data.results : [...prevMovies, ...data.results]
-      );
-      setTotalPages(data.total_pages);
-      setShouldFetch(false); // Disable fetch after initial load
-    }
-  }, [data]);
+    useEffect(() => {
+        if (data) {
+            setMovies((prevMovies) => (page === 1 ? data.results : [...prevMovies, ...data.results]));
+            setTotalPages(data.total_pages);
+            setShouldFetch(false); // Disable fetch after initial load
+        }
+    }, [data]);
 
-  useEffect(() => {
-    setMovies([]);
-    setPage(1);
-    setShouldFetch(true); // Enable fetch for new genre
-  }, [inputGenre]);
+    useEffect(() => {
+        setMovies([]);
+        setPage(1);
+        setShouldFetch(true); // Enable fetch for new genre
+    }, [props.inputGenre]);
 
-  const loadMoreMovies = () => {
-    setPage((prevPage) => prevPage + 1);
-    setShouldFetch(true); // Enable fetch for next page
-  };
+    const loadMoreMovies = () => {
+        setPage((prevPage) => prevPage + 1);
+        setShouldFetch(true); // Enable fetch for next page
+    };
 
-  return (
-    <section className="movie-genre-list">
-      {movies.map((singleMovie: IMovieHome) => (
-        <MovieCard
-          singleMovie={singleMovie}
-          inputGenre={inputGenre}
-          key={singleMovie.id}
-        />
-      ))}
-      {page < totalPages && (
-        <button onClick={loadMoreMovies} className="load-more-button">
-          Load More
-        </button>
-      )}
-    </section>
-  );
-};
+    return (
+        <section className="movie-genre-list" ref={ref}>
+            {movies.map((singleMovie: IMovieHome) => (
+                <MovieCard singleMovie={singleMovie} inputGenre={props.inputGenre} key={singleMovie.id} />
+            ))}
+            {page < totalPages && (
+                <button onClick={loadMoreMovies} className="load-more-button">
+                    Load More
+                </button>
+            )}
+        </section>
+    );
+});
 
 export default MovieList;
--- src/pages/searchPage/SearchPage.tsx
+++ src/pages/searchPage/SearchPage.tsx
@@ -1,24 +1,28 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import GenreSlider, { IGenre } from "../../components/genreSlider/GenreSlider";
 
 import Searchbar from "../../components/searchbar/Searchbar";
 import "./SearchPage.css";
 import MovieList from "../../components/movieList/MovieList";
 import Footer from "../../components/footer/Footer";
+import BackToTopButton from "../../components/backToTopButton/BackToTopButton";
 
 interface ISearchPageProps {
-  inputGenre: IGenre;
-  setInputGenre: React.Dispatch<React.SetStateAction<IGenre>>;
+    inputGenre: IGenre;
+    setInputGenre: React.Dispatch<React.SetStateAction<IGenre>>;
 }
 const SearchPage: FC<ISearchPageProps> = ({ setInputGenre, inputGenre }) => {
-  return (
-    <section className="search-page">
-      <Searchbar />
-      <GenreSlider setInputGenre={setInputGenre} />
-      <MovieList inputGenre={inputGenre} />
-      <Footer />
-    </section>
-  );
+    const listRef = useRef(null);
+
+    return (
+        <section className="search-page">
+            <Searchbar />
+            <GenreSlider setInputGenre={setInputGenre} />
+            <BackToTopButton listRef={listRef} />
+            <MovieList inputGenre={inputGenre} ref={listRef} />
+            <Footer />
+        </section>
+    );
 };
 
 export default SearchPage;
