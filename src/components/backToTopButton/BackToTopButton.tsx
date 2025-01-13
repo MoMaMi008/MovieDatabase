cdae1c0a143cb19a420414dd280f0e8b75448890
Moritz Reinhold <reinhold.moritz887@gmail.com>
2025-01-13 16:01:49 +0100

    finish: BackToTopButton

--- src/components/backToTopButton/BackToTopButton.tsx
+++ src/components/backToTopButton/BackToTopButton.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ArrowUp from "../../assets/SVG/Arrow-Up.svg";
+import { motion } from "motion/react";
 import "./BackToTopButton.css";
 
 interface BackToTopButtonProps {
@@ -39,13 +40,15 @@ const BackToTopButton = React.forwardRef<HTMLButtonElement, BackToTopButtonProps
     }, [listRef]);
 
     return (
-        isVisible && (
-            <button className="backToTopButton" onClick={backToTop} ref={ref}>
-                <img src={ArrowUp} alt="Arrow Icon" />
-                <p>Back to Top</p>
-                <img src={ArrowUp} alt="Arrow Icon" />
-            </button>
-        )
+        <motion.div initial={{ height: 0 }} animate={{ height: isVisible ? "fit-content" : 0, visibility: isVisible ? "visible" : "hidden" }} exit={{ height: 0 }} transition={{ duration: 0.2 }} className="back">
+            <motion.button ref={ref} className="backToTopButton" onClick={backToTop} style={{ padding: isVisible ? "5px 10px" : 0 }} initial={{ height: 0 }} animate={{ height: isVisible ? "100%" : 0 }} transition={{ duration: 0.2 }}>
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: isVisible ? "100%" : 0 }} transition={{ duration: 0 }}>
+                    <img src={ArrowUp} alt="Arrow Icon" />
+                    <p>Back to Top</p>
+                    <img src={ArrowUp} alt="Arrow Icon" />
+                </motion.div>
+            </motion.button>
+        </motion.div>
     );
 });
 
